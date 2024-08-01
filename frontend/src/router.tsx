2966596089d0c6f8cba2925f612aec5b5dd3de18
jgezziel <gzl1910@gmail.com>
2024-08-01 11:59:03 -0600

    add products router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./views/Layout";
 import Index from "./views/Index";
 import Customers from "./views/Customers";
+import Products from "./views/Products";
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Index />} />
           <Route path="customers" element={<Customers />} />
+          <Route path="products" element={<Products />} />
         </Route>
       </Routes>
     </BrowserRouter>

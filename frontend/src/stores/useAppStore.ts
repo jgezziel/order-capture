17f09a815bd636cb0789ada2f7ec4a504c544870
jgezziel <gzl1910@gmail.com>
2024-08-01 11:59:38 -0600

    update appstore

--- frontend/src/stores/useAppStore.ts
+++ frontend/src/stores/useAppStore.ts
@@ -1,9 +1,18 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createCustomersSlice, type customersSliceType } from "./customerSlice";
+import { createProductsSlice, type productsSliceType } from "./productSlice";
+import {
+  createShippingAddressSlice,
+  type shippingAddressSliceType,
+} from "./shippingAddressSlice";
 
-export const useAppStore = create<customersSliceType>()(
+export const useAppStore = create<
+  customersSliceType & productsSliceType & shippingAddressSliceType
+>()(
   devtools((...a) => ({
     ...createCustomersSlice(...a),
+    ...createProductsSlice(...a),
+    ...createShippingAddressSlice(...a),
   }))
 );

1ebeabbc6e46381161135cf083a6bdbb61b75378
Geziel Balan <gzl1910@gmail.com>
2024-08-04 18:00:11 -0600

    add docker frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 4000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:4000",
+  },
+});
